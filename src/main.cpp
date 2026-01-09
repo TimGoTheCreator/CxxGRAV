6d39e228d8d1b07c96e0c2650527258657e65ad1
TimGoTheCreator <timgooffical@gmail.com>
2026-01-09 19:52:05 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,56 @@
-#include"conf.h"
-#include"raylib.h"
+#include "conf.h"
+#include "raylib.h"
+#include <vector>
+#include <string>
 
 int main() {
     InitWindow(800, 600, "SemiEuler");
-    Particle earth(0, 0, 0, 0, 5.97e24);
-    Particle moon(384400000, 0, 0, 1022, 7.35e22);
-    SetTargetFPS(0); // run as fast as the cpu can :)
+    std::vector<Particle> particles;
+    particles.emplace_back(0, 0, 0, 0, 1.99e30, 6.96e8, "Sun");
+    particles.emplace_back(150000000000, 0, 0, 29780, 5.97e24, 6.37e6, "Earth");
+    particles.emplace_back(150384400000, 0, 0, 30802, 7.35e22, 1.74e6, "Moon");
+
+
+    SetTargetFPS(0);
 
     double dt = 0.4;
     double simTime = 0.0;
+    double scale = 1.5e6;
+    int steps = 10000;
 
     while (!WindowShouldClose()) {
-        for(int i = 0; i < 10000; i++) {
-            Gravity(earth, moon, dt);
-            earth.x += earth.vx * dt;
-            earth.y += earth.vy * dt;
-            moon.x += moon.vx * dt;
-            moon.y += moon.vy * dt;
-            simTime += dt;
-            }
-        
+        if (IsKeyDown(KEY_W)) scale *= 1.005;
+        if (IsKeyDown(KEY_S)) scale /= 1.005;
 
-        double scale = 1.5e6;
+        for (int s = 0; s < steps; s++) {
+            for (size_t i = 0; i < particles.size(); i++) {
+                for (size_t j = i + 1; j < particles.size(); j++) {
+                    Gravity(particles[i], particles[j], dt);
+                }
+            }
+
+            for (auto& p : particles) {
+                p.x += p.vx * dt;
+                p.y += p.vy * dt;
+            }
+
+            simTime += dt;
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawCircle((int)(earth.x / scale) + 400, (int)(earth.y / scale) + 300, 10, BLUE);
-        DrawCircle((int)(moon.x / scale) + 400, (int)(moon.y / scale) + 300, 5, GRAY);
+        for (auto& p : particles) {
+
+            int radius = (int)(p.r / scale);
+            if (radius < 1) radius = 1;
+
+            Color color = WHITE;
+            if (p.name == "Earth") color = BLUE;
+            else if (p.name == "Sun") color = YELLOW;
+            else if (p.name == "Moon") color = GRAY;
+
+            DrawCircle((int)(p.x / scale) + 400, (int)(p.y / scale) + 300, radius, color);
+        }
         EndDrawing();
     }
 }
